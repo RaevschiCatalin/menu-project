73a9ea1b5d1b636fec2cbeca368ab7ffe36c8cee
PinkPlush <raevschicatalin@gmail.com>
2023-04-05 14:03:14 +0300

    importing alpha version

---
+++ main.c
@@ -0,0 +1,372 @@
+#include <ncurses.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int highlight = 1;
+int choice = 0;
+int c;
+char *menu_items[] = {
+        "  Adauga un event",
+        "  Sterge un event",
+        "  Vizualizarea evenimentelor",
+        "  Cauta o intalnire",
+        "  Iesi"
+    };
+
+
+int menu_size = sizeof(menu_items) / sizeof(menu_items[0]);
+typedef struct {
+    char date[80];
+    char hour[80];
+    char name[80];
+    char place[80];
+} meeting;
+char* get_current_datetime() {
+    time_t t = time(NULL);
+    struct tm *tm = localtime(&t);
+    static char datetime[80];
+    strftime(datetime, sizeof(datetime), "%Y-%m-%d %H:%M", tm);
+    return datetime;
+}
+
+int is_valid_date(char *date) {
+    // Get the current time
+    time_t now = time(NULL);
+    struct tm *tm = localtime(&now);
+
+    // Add one month to the current time
+    tm->tm_mon += 1;
+    time_t one_month_from_now = mktime(tm);
+
+    // Convert the entered date to a time_t
+    struct tm entered_date = {0};
+    strptime(date, "%Y-%m-%d", &entered_date);
+    time_t entered_time = mktime(&entered_date);
+
+    // Check if the entered date is more than a month from now
+    return difftime(entered_time, one_month_from_now) <= 0;
+}
+
+void main_menu(int hi){
+        clear();
+        char *datetime = get_current_datetime();
+        printw("============Meniul Principal============\n");
+        printw("== Utilizati sagetile pentru navigare ==\n");
+        printw("========================================\n");
+        printw("============%s============\n",datetime);
+        printw("========================================\n\n");
+        for (int i = 0; i < menu_size; i++) {
+            if (i + 1 == hi) {
+                attron(A_REVERSE);//highlight la alegere
+            }
+            printw("%s\n",menu_items[i]);
+            attroff(A_REVERSE);//hz
+        }
+        return;
+}
+int choose(){
+
+    while (1) {
+        main_menu(highlight);
+        c = getch();
+        switch(c) {
+            case 'w':
+            case KEY_UP:
+                if (highlight > 1)
+                    highlight--;
+                break;
+            case 's':
+            case KEY_DOWN:
+                if (highlight < menu_size)
+                    highlight++;
+                    choice++;
+                break;
+            case 'd':
+            case KEY_RIGHT:
+                if (highlight < menu_size)
+                    highlight++;
+                break;
+            case 'a':
+            case KEY_LEFT:
+                if (highlight > 1)
+                    highlight--;
+                break;
+             case '\n':
+            case KEY_ENTER:
+                choice = highlight;
+
+                return choice;
+        }
+    }
+    return 0;
+}
+
+void add_meeting(){
+    clear();
+    echo();
+    meeting m;
+    attron(COLOR_PAIR(1));
+    mvprintw(1,15,"================================================\n");
+    mvprintw(2,15,"========== Introdu data evenimentului ==========\n");
+    mvprintw(3,15,"====== Formatul trebuie sa fie YYYY-MM-DD ======\n");
+    mvprintw(4,15,">>> ");
+    getstr(m.date);
+    if (!is_valid_date(m.date)) {
+        mvprintw(5,15,"Data introdusa nu imi place\n");
+        getch();
+        return;
+    }
+    attroff(COLOR_PAIR(1));
+    attron(COLOR_PAIR(2));
+    mvprintw(6,15,"========== Introdu ora evenimentului ===========\n");
+    mvprintw(7,15,">>> ");
+    getstr(m.hour);
+    attroff(COLOR_PAIR(2));
+    attron(COLOR_PAIR(3));
+    mvprintw(8,15,"======== Introdu denumirea evenimentului =======\n");
+    mvprintw(9,15,">>> ");
+    getstr(m.name);
+    attroff(COLOR_PAIR(3));
+    mvprintw(10,15,"========== Introdu locul evenimentului =========\n");
+    mvprintw(11,15,">>> ");
+    getstr(m.place);
+
+    // Open the text file in append mode
+    FILE *fp = fopen("events.txt", "a");
+    if (fp == NULL) {
+        printw("Error opening file!\n");
+        return;
+    }
+
+    // Write the event details to the file
+    fprintf(fp, "%s %s %s %s\n", m.date, m.hour, m.name, m.place);
+
+    // Close the file
+    fclose(fp);
+
+   mvprintw(11,15," Apasa Backspace pentru a reveni la Meniul Principal");
+   int c = getch();
+   if(c == KEY_BACKSPACE)
+       choose();
+
+   return;
+}
+
+void delete_meeting(){
+    clear();
+    echo();
+    char name[80];
+    int found = 0;
+    mvprintw(1,15,"================================================\n");
+    mvprintw(2,15,"============== Vizualizarea evenimentelor ======\n");
+
+    // Open the text file in read mode
+    FILE *fp = fopen("events.txt", "r");
+    if (fp == NULL) {
+        printw("Error opening file!\n");
+        return;
+    }
+
+    // Read and display each event from the file
+    int line = 4;
+    meeting m;
+    while (fscanf(fp, "%s %s %s %s\n", m.date, m.hour, m.name, m.place) == 4) {
+        mvprintw(line++, 15, "Data: %s Ora: %s Numele: %s Locul: %s\n", m.date, m.hour, m.name, m.place);
+    }
+
+   // Close the file
+   fclose(fp);
+    mvprintw(line + 1,15,"============== Ce vrei sa stergi? ==============\n");
+    mvprintw(line + 2,15,"Scrie numele evenimentului sau apasa ~ pentru a iesi\n");
+   while (!found) {
+
+       mvprintw(line + 3,15,">>>");
+       getstr(name);
+
+       if (strcmp(name, "~") == 0) {
+           choose();
+           return;
+       }
+
+       // Open the text file in read mode
+       fp = fopen("events.txt", "r");
+       if (fp == NULL) {
+           printw("Error opening file!\n");
+           return;
+       }
+
+       // Check if the event name exists in the text file
+       while (fscanf(fp, "%s %s %s %s\n", m.date, m.hour, m.name, m.place) == 4) {
+           if (strcmp(m.name, name) == 0) {
+               found = 1;
+               break;
+           }
+
+
+       // Close the file
+       fclose(fp);
+
+       if (!found) {
+           mvprintw(line + 7,15,"Numele evenimentului nu a fost gasit. Incearca din nou.\n");
+           line++;
+       }
+   }
+}
+   // Create a temporary file to store the updated events
+   FILE *tmp = fopen("tmp.txt", "w");
+   if (tmp == NULL) {
+       printw("Error creating temporary file!\n");
+       fclose(fp);
+       return;
+   }
+
+   // Open the text file in read mode
+   fp = fopen("events.txt", "r");
+   if (fp == NULL) {
+       printw("Error opening file!\n");
+       fclose(tmp);
+       return;
+   }
+
+   // Read each event from the text file
+   while (fscanf(fp, "%s %s %s %s\n", m.date, m.hour, m.name, m.place) == 4) {
+       // If the event name doesn't match the one to be deleted,
+       // write it to the temporary file
+       if (strcmp(m.name, name) != 0) {
+           fprintf(tmp, "%s %s %s %s\n", m.date, m.hour, m.name, m.place);
+       }
+   }
+
+   // Close both files
+   fclose(fp);
+   fclose(tmp);
+
+   // Delete the original text file and rename the temporary file
+   remove("events.txt");
+   rename("tmp.txt", "events.txt");
+    mvprintw(line+6,15," Evenimentul '%s' a fost sters",name);
+   mvprintw(line + 7 ,15," Apasa Backspace pentru a reveni la Meniul Principal");
+   int c = getch();
+   if(c == KEY_BACKSPACE)
+       choose();
+
+   return;
+}
+
+
+void view(){
+    clear();
+    echo();
+    char str[80];
+    mvprintw(1,15,"================================================\n");
+    mvprintw(2,15,"============== Vizualizarea evenimentelor ======\n");
+
+    // Open the text file in read mode
+    FILE *fp = fopen("events.txt", "r");
+    if (fp == NULL) {
+        printw("Error opening file!\n");
+        return;
+    }
+
+    // Read and display each event from the file
+    int line = 4;
+    meeting m;
+    while (fscanf(fp, "%s %s %s %s\n", m.date, m.hour, m.name, m.place) == 4) {
+        mvprintw(line++, 15, "Data: %s Ora: %s Denumirea: %s Locul: %s\n", m.date, m.hour, m.name, m.place);
+    }
+
+   // Close the file
+   fclose(fp);
+
+   int j = getch();
+   while(j != KEY_ENTER){
+       getstr(str);
+       break;
+   }
+
+   mvprintw(line + 2, 15," Apasa Backspace pentru a reveni la Meniul Principal");
+   int c = getch();
+   if(c == KEY_BACKSPACE)
+       choose();
+
+   return;
+}
+void search_meetings(){
+    clear();
+    echo();
+    char date[80];
+    mvprintw(1,15,"================================================\n");
+    mvprintw(2,15,"============== Cauta evenimente ================\n");
+    mvprintw(3,15,"Introdu data evenimentelor pe care vrei sa le cauti (YYYY-MM-DD):\n");
+    mvprintw(4,15,">>>");
+    getstr(date);
+
+    // Open the text file in read mode
+    FILE *fp = fopen("events.txt", "r");
+    if (fp == NULL) {
+        printw("Error opening file!\n");
+        return;
+    }
+
+    // Read and display each event from the file
+    int line = 6;
+    meeting m;
+    int flag = 0;
+    while (fscanf(fp, "%s %s %s %s\n", m.date, m.hour, m.name, m.place) == 4) {
+        if (strcmp(m.date, date) == 0) {
+            mvprintw(line++, 15, "Data: %s Ora: %s Denumirea: %s Locul: %s\n", m.date, m.hour, m.name, m.place);
+            flag = 1;
+        }
+
+    }
+    if(flag == 0){
+    mvprintw(line,15," Nu am gasit nici un element la aceasta data :(((((((");
+    }
+
+   // Close the file
+   fclose(fp);
+   mvprintw(line + 1, 15," Apasa Backspace pentru a reveni la Meniul Principal");
+   int c = getch();
+   if(c == KEY_BACKSPACE)
+       choose();
+
+   return;
+}
+int main() {
+    initscr();
+    cbreak();
+    noecho();
+    curs_set(0);
+    start_color();
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    init_pair(3, COLOR_BLUE, COLOR_BLACK);
+    keypad(stdscr, TRUE);
+    int aleg = choose();
+    while (aleg != 0) {
+        switch(aleg) {
+            case 1:
+                add_meeting();
+                break;
+            case 2:
+                delete_meeting();
+                break;
+            case 3:
+                view();
+                break;
+            case 4:
+                search_meetings();
+                break;
+            case 5:
+                endwin();
+                exit(0);
+            default:
+                break;
+        }
+        aleg = choose();
+    }
+
+
+    return 0;
+}
